6b9b102e5c9d85954b3256eda12016ff5273a557
Bryan Duggan <skooter500@gmail.com>
2023-02-14 13:00:58 +0000

    trails

--- GodotBoids/Boid.gd
+++ GodotBoids/Boid.gd
@@ -36,7 +36,12 @@ func _drawGizmos():
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():	
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+	
 	targetNode = get_node(targetNodePath)
 	if pathFollowEnabled:
 		path = $"../Path".get_curve()
