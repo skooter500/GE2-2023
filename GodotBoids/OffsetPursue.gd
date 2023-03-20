993a30d4854783e4a3c6a36629e960057f148cb9
Bryan Duggan <skooter500@gmail.com>
2023-03-20 20:52:39 +0000

    enable disable

--- GodotBoids/OffsetPursue.gd
+++ GodotBoids/OffsetPursue.gd
@@ -1,27 +1,27 @@
 extends SteeringBehavior
 
-export var leaderNodePath:NodePath
-var leaderBoid:Node
-var leaderOffset:Vector3
+export var leader_node_path:NodePath
+var leader_boid:Node
+var leader_offset:Vector3
 var target:Vector3
-var worldTarget:Vector3
+var world_target:Vector3
 var projected:Vector3
 	
 func _ready():
 	boid = get_parent()
-	leaderBoid = get_node(leaderNodePath)
-	leaderOffset = boid.global_transform.origin - leaderBoid.global_transform.origin
-	leaderOffset = leaderBoid.global_transform.basis.xform_inv(leaderOffset)
+	leader_boid = get_node(leader_node_path)
+	leader_offset = boid.global_transform.origin - leader_boid.global_transform.origin
+	leader_offset = leader_boid.global_transform.basis.xform_inv(leader_offset)
 	
 func _process(delta):
-	if drawGizmos:
-		DebugDraw.draw_line(projected, worldTarget, Color.aliceblue)			
+	if draw_gizmos:
+		DebugDraw.draw_line(projected, world_target, Color.aliceblue)			
 
 func calculate():		
-	worldTarget = leaderBoid.global_transform.xform(leaderOffset)
-	var dist = boid.global_transform.origin.distance_to(worldTarget)
+	world_target = leader_boid.global_transform.xform(leader_offset)
+	var dist = boid.global_transform.origin.distance_to(world_target)
 	var time = dist / boid.max_speed
-	projected = worldTarget + leaderBoid.velocity * time
+	projected = world_target + leader_boid.velocity * time
 	
 
 	return boid.arrive_force(projected, 5)
