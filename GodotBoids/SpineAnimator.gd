c1cd717ff25e68cbc246f77159523837480af221
Bryan Duggan <skooter500@gmail.com>
2023-03-28 12:05:38 +0100

    move & slide 1

--- GodotBoids/SpineAnimator.gd
+++ GodotBoids/SpineAnimator.gd
@@ -35,6 +35,7 @@ func _physics_process(delta):
 		var lerped = lerp(next.global_transform.origin, wantedPos, delta * damping)
 		var clamped = (lerped - prev.global_transform.origin).normalized() * offsets[i].length()
 		var pos = prev.global_transform.origin + clamped
+		# next.move_and_slide(pos - next.global_transform.origin)
 		next.global_transform.origin = pos
 		
 		var prevRot = prev.global_transform.basis.orthonormalized()
