9c2450b1bf30b80f339dd3a4e18548a31de28ca1
Bryan Duggan <skooter500@gmail.com>
2023-04-03 00:17:36 +0100

    boids yes yes yesy yes

--- GodotBoids/Cohesion.gd
+++ GodotBoids/Cohesion.gd
@@ -9,12 +9,8 @@ func _ready():
 	boid.count_neighbors = true
 
 func draw_gizmos():
-	for i in boid.neighbors.size():
-		var other = boid.neighbors[i]
-		var to_boid = boid.transform.origin - boid.neighbors[i].transform.origin
-		to_boid = to_boid.normalized()
-		DebugDraw.draw_arrow_line(other.global_transform.origin, other.global_transform.origin + to_boid * force.length() * weight * 2, Color.darkseagreen, 0.1)
-
+	DebugDraw.draw_arrow_line(boid.transform.origin, center_of_mass, Color.darkseagreen, 0.1)
+	
 func calculate():
 	force = Vector3.ZERO
 	center_of_mass = Vector3.ZERO
