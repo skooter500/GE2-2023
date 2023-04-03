7c40a3498a033fb90c8d6268e29c6fb53c705e07
Bryan Duggan <skooter500@gmail.com>
2023-04-03 12:13:59 +0100

    draw the box

--- GodotBoids/School.gd
+++ GodotBoids/School.gd
@@ -20,7 +20,7 @@ func draw_gizmos():
 		var size = 1000
 		var sub_divisions = size / cell_size
 		DebugDraw.draw_grid(Vector3.ZERO, Vector3.RIGHT * size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.aquamarine)
-		DebugDraw.draw_grid(Vector3.ZERO, Vector3.UPd * size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.aquamarine)
+		DebugDraw.draw_grid(Vector3.ZERO, Vector3.UP * size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.aquamarine)
 
 func position_to_cell(pos, cell_size, grid_size = 10000):        
 	var p = Vector3( 
