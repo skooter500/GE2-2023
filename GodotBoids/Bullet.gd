1d5b3a53c26965a2efe5ae24f146ad4e01e4886b
Bryan Duggan <skooter500@gmail.com>
2023-04-12 14:44:37 +0100

    bullet with trails wip

--- GodotBoids/Bullet.gd
+++ GodotBoids/Bullet.gd
@@ -6,26 +6,36 @@ export var speed = 10.0
 # var a = 2
 # var b = "text"
 
+var noise:OpenSimplexNoise = OpenSimplexNoise.new()
+
+func _ready():
+	noise.seed = randi()
+	noise.octaves = 4
+	noise.period = 20.0
+	noise.persistence = 0.8
+
 func destroy():
 	get_parent().remove_child(self)
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 	
+	
+var t = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var v  = speed * transform.basis.z
+	var v  = speed * transform.basis.z * delta
+		
 	
 	# Add a noise 
+	var n = Vector3(noise.get_noise_1d(t), noise.get_noise_1d(t + 1),  noise.get_noise_1d(t + 2))
+	n = global_transform.basis.xform(n)
 	
 	# I think this should return true when a collision happens, but it doesnt
-	var collision = move_and_collide(v * delta)		
-	if collision:
-		print("Collision!!!!")
-
+	# var collision = move_and_collide(v * delta)		
+	translate(v + n)
+	# look_at(global_transform.origin - v, global_transform.basis.y)
+	# if collision:
+	# 	print("Collision!!!!")
+	t += delta
 
 func _on_Timer_timeout():
 	destroy()
